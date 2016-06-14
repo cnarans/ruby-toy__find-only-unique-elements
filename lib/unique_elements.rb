e1fbf383c457343888389b2c570686e50d2ee28c
Colin Narans <cnarans@gmail.com>
2016-06-14 00:44:19 +0000

    submitting ruby_unique

--- lib/unique_elements.rb
+++ lib/unique_elements.rb
@@ -2,5 +2,11 @@
 # combination of the above), and returns an array of the elements that appear once and only
 # once.
 def find_unique_elements(arr)
-  
+    x = []
+    for i in 0..arr.length-1
+        if arr.count(arr[i])==1
+            x.push(arr[i])
+        end
+    end
+    return x
 end
